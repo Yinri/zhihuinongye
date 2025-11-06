eaf7b5cbeea6a9f47b83c6f2560ab88326ddc322
zhangyingjian <2966890768@qq.com>
2025-11-06 19:11:43 +0800

    虫害后端

--- JeecgBoot-master/jeecg-boot/jeecg-module-youcai/src/main/java/org/jeecg/modules/youcai/mapper/YoucaiPesticideInputsMapper.java
+++ JeecgBoot-master/jeecg-boot/jeecg-module-youcai/src/main/java/org/jeecg/modules/youcai/mapper/YoucaiPesticideInputsMapper.java
@@ -1,7 +1,10 @@
 package org.jeecg.modules.youcai.mapper;
 
+import org.apache.ibatis.annotations.Select;
 import org.jeecg.modules.youcai.entity.YoucaiPesticideInputs;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import java.util.List;
 
 /**
  * @Description: 农药投入表
@@ -10,5 +13,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @Version: V1.0
  */
 public interface YoucaiPesticideInputsMapper extends BaseMapper<YoucaiPesticideInputs> {
-
+    @Select("SELECT DISTINCT pesticide_name FROM youcai_pesticide_inputs WHERE del_flag = 0")
+    List<String> selectDistinctPesticideNames();
 }
--- JeecgBoot-master/jeecg-boot/jeecg-module-youcai/src/main/java/org/jeecg/modules/youcai/service/impl/YoucaiPesticideInputsServiceImpl.java
+++ JeecgBoot-master/jeecg-boot/jeecg-module-youcai/src/main/java/org/jeecg/modules/youcai/service/impl/YoucaiPesticideInputsServiceImpl.java
@@ -3,9 +3,12 @@ package org.jeecg.modules.youcai.service.impl;
 import org.jeecg.modules.youcai.entity.YoucaiPesticideInputs;
 import org.jeecg.modules.youcai.mapper.YoucaiPesticideInputsMapper;
 import org.jeecg.modules.youcai.service.IYoucaiPesticideInputsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+import java.util.List;
 
 /**
  * @Description: 农药投入表
@@ -15,5 +18,10 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
  */
 @Service
 public class YoucaiPesticideInputsServiceImpl extends ServiceImpl<YoucaiPesticideInputsMapper, YoucaiPesticideInputs> implements IYoucaiPesticideInputsService {
-
+    @Autowired
+    private YoucaiPesticideInputsMapper youcaiPesticideInputsMapper;
+    @Override
+    public List<String> listDistinctPesticideNames() {
+        return youcaiPesticideInputsMapper.selectDistinctPesticideNames();
+    }
 }
