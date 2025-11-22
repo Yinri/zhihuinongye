de0ecf7d72ae19d3412d19dd12034a5c50e039df
cyx <2433209461@qq.com>
2025-11-22 14:06:48 +0800

    农药信息管理后端代码

---
+++ JeecgBoot-master/jeecg-boot/jeecg-module-youcai/src/main/java/org/jeecg/modules/youcai/service/IYoucaiPesticideInfoService.java
@@ -0,0 +1,15 @@
+package org.jeecg.modules.youcai.service;
+
+import org.jeecg.modules.youcai.entity.YoucaiPesticideInfo;
+import com.baomidou.mybatisplus.extension.service.IService;
+import java.util.List;
+
+/**
+ * @Description: 农药信息表
+ * @Author: jeecg-boot
+ * @Date:   2025-10-30
+ * @Version: V1.0
+ */
+public interface IYoucaiPesticideInfoService extends IService<YoucaiPesticideInfo> {
+
+}
