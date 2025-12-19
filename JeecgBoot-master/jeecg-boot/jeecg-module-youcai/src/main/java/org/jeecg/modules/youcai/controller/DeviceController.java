11868eddcd477b431ec8fa82eb252229df7ed4d0
cyx <2433209461@qq.com>
2025-12-19 15:27:43 +0800

    数据对接

--- JeecgBoot-master/jeecg-boot/jeecg-module-youcai/src/main/java/org/jeecg/modules/youcai/controller/DeviceController.java
+++ JeecgBoot-master/jeecg-boot/jeecg-module-youcai/src/main/java/org/jeecg/modules/youcai/controller/DeviceController.java
@@ -15,16 +15,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @Description: 设备状态管理
+ * @Description: 设备管理
  * @Author: jeecg-boot
  * @Date: 2025-12-18
  * @Version: V1.0
  */
-@Tag(name="设备状态管理")
+@Tag(name="设备管理")
 @RestController
 @RequestMapping("/rapeseed/device")
 @Slf4j
-public class DeviceStatusController {
+public class DeviceController {
 
     @Autowired
     private IoTApiUtil ioTApiUtil;
@@ -35,11 +35,11 @@ public class DeviceStatusController {
      * @param projectId 项目ID，默认为1
      * @return 设备状态信息
      */
-    @AutoLog(value = "设备状态管理-获取设备状态")
-    @Operation(summary="设备状态管理-获取设备状态", description="设备状态管理-获取设备状态")
+    @AutoLog(value = "设备管理-获取设备状态")
+    @Operation(summary="设备管理-获取设备状态", description="设备管理-获取设备状态")
     @GetMapping(value = "/status")
     public Result<Object> getDeviceStatus(
-            @Parameter(name="projectId", description="项目ID") @RequestParam(name="projectId", defaultValue = "1") Integer projectId) {
+            @Parameter(name="projectId", description="项目ID") @RequestParam(name="projectId", defaultValue = "229") Integer projectId) {
         try {
             log.info("获取项目ID {} 的设备状态信息", projectId);
             
